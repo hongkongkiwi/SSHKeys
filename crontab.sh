e7c966bab0e7b45f77b110c821d2300d84c01623
Andy <andy@savage.hk>
2015-07-14 19:30:28 +0800

    Updated config values

--- crontab.sh
+++ crontab.sh
@@ -8,27 +8,21 @@
 CONFIG_FILE=${1:-"$HOME/.ssh/sshkeys.conf"}
 
 # Takes a config file and sources it with a few simple checks
-read_config () {
-    if [ ! -f "$1" ]; then
-        echo "Config file $CONFIG_FILE not found!"
-        exit 1
-    fi
+if [ ! -f "$CONFIG_FILE" ]; then
+    echo "Config file $CONFIG_FILE not found!"
+    exit 1
+fi
 
-    # now source it, either the original or the filtered variant
-    source "$1"
-}
-
-read_config "$CONFIG_FILE"
+# now source it, either the original or the filtered variant
+source "$CONFIG_FILE"
 
 GIT_BIN=${GIT_BIN:-"/usr/local/bin/git"}
 
-if [[ "$REPO_URL" == "" ]]; then
-    echo "REPO_URL variable in config file is blank!"
-    exit 2
-fi
+[ "$REPO_URL" == "" ] && echo "REPO_URL variable in config file is blank!" && exit 2
+[ "$REPO_PATH" == "" -o ! -d "$REPO_PATH" ] && echo "REPO_PATH variable in config file is invalid or blank!" && exit 2
 
 # Get changes from remote and reset local changes
-"$GIT_BIN" -C "$REPO_URL" fetch --quiet --all && "$GIT_BIN" -C "$REPO_URL" reset --quiet --hard origin/master
+"$GIT_BIN" -C "$REPO_PATH" fetch --quiet --all && "$GIT_BIN" -C "$REPO_PATH" reset --quiet --hard origin/master
 
 # Exit based on last command
 exit $?
