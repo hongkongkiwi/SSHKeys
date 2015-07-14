39daaab07f6319cacc5d123564c7eb87535a5634
Andy <andy@savage.hk>
2015-07-14 18:32:00 +0800

    Updated crontab to be a bit smarter

--- crontab.sh
+++ crontab.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+
+##
+# This script updates the authorized keys file regularly from our git repo
+##
+
 # Get script Directory
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+GIT_BIN="/usr/bin/git"
+REPO_PATH="$HOME/.ssh/SSHKeys"
 
-# Ensure we are in the script directory
-cd "$DIR"
 # Get changes from remote and reset local changes
-/usr/bin/git fetch --quiet --all && git reset --quiet --hard origin/master
+"$GIT_BIN" -C "$REPO_PATH" fetch --quiet --all && "$GIT_BIN" -C "$REPO_PATH" reset --quiet --hard origin/master
 
 # Exit based on last command
 exit $?
